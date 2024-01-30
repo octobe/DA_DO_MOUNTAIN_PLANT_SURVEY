aca9a08b0ff69c2937acfea653a7e247460ecbb8
octobe <61076100+octobe@users.noreply.github.com>
2024-01-30 16:47:30 +0800

    Update script.js
    when click the link on the sidebar, hides the sidebar.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
     // 監聽側邊欄連結的點擊事件
-    $('#sidebarNav .sidebar a').click(function () {
+    $('.sidebar a').click(function () {
       // 觸發 navbar-toggler 按鈕的點擊事件
-      $('#sidebarNav .navbar-toggler').click();
+      $('.navbar-toggler').click();
     });
-  
+
     // 監聽 sidebarNav 的展開事件
     $('#sidebarNav').on('show.bs.collapse', function () {
       // 切換 navbar-toggler-icon 的類別，讓圖示變成 X
-      $('#sidebarNav .navbar-toggler-icon').removeClass('navbar-toggler-icon').addClass('navbar-toggler-icon-x');
+      $('.navbar-toggler-icon').removeClass('navbar-toggler-icon').addClass('navbar-toggler-icon-x');
     });
-  
+
     // 監聽 sidebarNav 的折疊事件
     $('#sidebarNav').on('hide.bs.collapse', function () {
       // 切換 navbar-toggler-icon 的類別，還原原來的圖示
-      $('#sidebarNav .navbar-toggler-icon-x').removeClass('navbar-toggler-icon-x').addClass('navbar-toggler-icon');
+      $('.navbar-toggler-icon-x').removeClass('navbar-toggler-icon-x').addClass('navbar-toggler-icon');
     });
   });
   
