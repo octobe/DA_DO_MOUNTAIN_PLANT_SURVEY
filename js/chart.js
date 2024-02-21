3d36c133a502fa8086e4bbf7bd8a464ba26d48f3
octobe <61076100+octobe@users.noreply.github.com>
2024-02-21 14:31:16 +0800

    Update chart.js
    function toggleChartDisplay() {
      const chartBlock = document.querySelector('.chartblock');
      const button = document.querySelector('.config.display button');
      
      chartBlock.classList.toggle('dual-display');
      
      if (chartBlock.classList.contains('dual-display')) {
        button.textContent = '單排顯示';
      } else {
        button.textContent = '併排顯示';
      }
    }

--- js/chart.js
+++ js/chart.js
@@ -240,7 +240,15 @@ function formatDate(date) {
 }
 function toggleChartDisplay() {
   const chartBlock = document.querySelector('.chartblock');
+  const button = document.querySelector('.config.display button');
+  
   chartBlock.classList.toggle('dual-display');
+  
+  if (chartBlock.classList.contains('dual-display')) {
+    button.textContent = '單排顯示';
+  } else {
+    button.textContent = '併排顯示';
+  }
 }
 
 // Call fetchData to initialize the existing chart
